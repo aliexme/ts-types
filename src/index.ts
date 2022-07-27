029ffb230cbc359879debf0623d7eee14ea04857
Alexander Smirnov <al.smirnov996@gmail.com>
2022-07-27 11:06:20 +0300

    feat: add Override type

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,5 @@ export type PartialProp<T, K extends keyof T> = Omit<T, K> & {
 export type RequiredProp<T, K extends keyof T> = Omit<T, K> & {
   [P in K]-?: T[P]
 }
+
+export type Override<T, U> = Omit<T, keyof T & keyof U> & U
